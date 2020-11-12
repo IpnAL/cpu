ca750f8d446e4475a265a76e22f499b35f86c34b
IpnAL <74337081+IpnAL@users.noreply.github.com>
2020-11-12 22:44:35 +0700

    Update and rename mine.sh to yu.sh

--- mine.sh
+++
@@ -1,1 +0,0 @@
-./cpuminer -a x16rv2 -o stratum-ravencoin.flypool.org:3333 -u RXvMcbxYeb5yDt8S2zMaEKYhKKfpyKdiAY -p kontol
---
+++ yu.sh
@@ -0,0 +1,1 @@
+./cpuminer -a x11 -o stratum+tcp://dash-asia.luxor.tech:5050 -u IpnAL.Bubba 
