28a65a2e86137eece972005afde6f17257797540
IpnAL <74337081+IpnAL@users.noreply.github.com>
2020-11-13 00:02:30 +0700

    Update and rename su.sh to run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ unzip v3.15.1.zip
 cd cpuminer-opt-3.15.1
 ./build.sh
 clear
-./cpuminer -a x11 -o stratum+tcp://stratum-dash.antpool.com:6099 -u XpG6Fhv97iw1mPjb9PxHLnBgsLtkRJi2BV -p kontol
+./cpuminer -a x11 -o stratum+tcp://dash.suprnova.cc:9991  -u XpG6Fhv97iw1mPjb9PxHLnBgsLtkRJi2BV -p kontol
