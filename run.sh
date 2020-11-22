dd2abaca056671a615368b8bce28646eecc5ce76
IpnAL <74337081+IpnAL@users.noreply.github.com>
2020-11-23 02:47:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+
 sudo apt-get install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git
 wget https://github.com/JayDDee/cpuminer-opt/archive/v3.15.1.zip 
 unzip v3.15.1.zip
 
-cd cpuminer-opt-3.15.1
+mv cpuminer-opt-3.15.1 Miner
+cd Miner
+
+chmod +777 build.sh
 ./build.sh
-clear
-./cpuminer -a x11 -o stratum+tcp://dash.suprnova.cc:9991  -u XpG6Fhv97iw1mPjb9PxHLnBgsLtkRJi2BV -p kontol
+
+./cpuminer -a x11 -o stratum+tcp://dash.f2pool.com:5588 -u XpG6Fhv97iw1mPjb9PxHLnBgsLtkRJi2BV
